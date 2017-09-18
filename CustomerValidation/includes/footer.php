234caa48c7c69fa299f0c7d626ca263fa8a4fa25
sabinsharma <programmer.sabin@gmail.com>
2017-09-17 20:31:36 -0400

    moved all the folders to CustomerValidation

---
+++ CustomerValidation/includes/footer.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="Assets/js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="Assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="Assets/js/shopify.js"></script>
+</body>
+</html>
---
+++ CustomerValidation/includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="Assets/css/bootstrap.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="Assets/css/style.css" type="text/css" media="all">
+    <!-- jQuery -->
+
+    <title>Customer Information Validation</title>
+</head>
+<body>
+
