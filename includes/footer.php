2a20a8d52d3b616ec0c8d17fd1cb35008639cefc
sabinsharma <programmer.sabin@gmail.com>
2017-09-17 20:28:20 -0400

    copied all files and folders to one folder CustomerValidation and update the changes

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript" src="Assets/js/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="Assets/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="Assets/js/shopify.js"></script>
-</body>
-</html>
